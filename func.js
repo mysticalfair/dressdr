6ffc6722d5610e619fdd45c43668c1263163eaea
Howard Huang <howardhuang96@gmail.com>
2017-12-23 01:05:50 -0800

    har har har

---
+++ func.js
@@ -0,0 +1,17 @@
+function on_start() {
+    console.log("call the command: hoho_and_jojo() for a special surprise <3")
+        
+}
+
+function hoho_and_jojo() {
+    console.log(`%c
+    < howie cowie loves joanna! <3 <3 <3 >
+     ----------------------------------------
+            \\   ^__^
+             \\  (oo)\\_______
+                (__)\\       )\\/\\
+                    ||----w |
+                    ||     ||`, "font-family:monospace")
+}
+
+on_start();
